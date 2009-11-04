672249cf6721e2f1d28b0376b0ff5788acc50001
Gabriel Olteanu <Gabriel Olteanu@f222f585-b4d1-44ad-a013-18d0c29269e2>
2009-11-04 00:43:34 +0000

    modification to Dinabotslave

---
+++ dinaBOT/DinaBOTSlave.java
@@ -0,0 +1,40 @@
+package dinaBOT;
+
+import lejos.nxt.Motor;
+import dinaBOT.mech.*;
+
+/**
+ * The DinaBOT class is the central class of our project. It ties everything togethere. It <b>is</b> the robot.
+ *
+ * @author Alexandre Courtemanche, Francois Ouellet Delorme, Gabriel Olteanu, Severin Smith, Stepan Salenikovich, Vinh Phong Buu
+*/
+public class DinaBOTSlave {
+	Stacking stacker;
+	
+	public DinaBOTSlave() {
+		stacker = new Stacker(Motor.A, Motor.B, Motor.C);
+	}
+	/**
+	 * This is our Hello World method. It will be gone soon
+	 *
+	*/
+	public void test() {
+		stacker.pickUp();
+		stacker.openDockingBay();
+		stacker.getCageStatus();
+		stacker.closeDockingBay();
+		stacker.getCageStatus();
+		
+	}
+	/**
+	 * This is where the static main method lies. This is where execution begins.
+	 *
+	 * @param args This is the command line args, this is irrelevent in the NXT
+	*/
+	public static void main(String[] args) {		
+		
+		DinaBOTSlave dinaBOTslave = new DinaBOTSlave();
+		dinaBOTslave.test();
+		while(true);
+	}
+}
