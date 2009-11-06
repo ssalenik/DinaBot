1171a4c0990257214c698d3aa5bef9e4455d14f6
severin.smith <severin.smith@f222f585-b4d1-44ad-a013-18d0c29269e2>
2009-11-06 23:56:57 +0000

    Updated MechConstants

--- dinaBOT/mech/MechConstants.java
+++ dinaBOT/mech/MechConstants.java
@@ -6,8 +6,8 @@ package dinaBOT.mech;
 */
 public interface MechConstants {
 	
-	static final double WHEEL_RADIUS = 2.7;
-	static final double WHEEL_BASE = 12.6;
+	static final double WHEEL_RADIUS = 2.659;
+	static final double WHEEL_BASE = 12.7435;
 	static final double SENSOR_BASE = 18;
 	
 	static double UNIT_TILE = 30.48;
