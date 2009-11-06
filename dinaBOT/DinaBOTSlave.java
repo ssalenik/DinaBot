8a9d97aa9e8842186d31828a2a4e7b9307a6c6e6
alexlesuper <alexlesuper@f222f585-b4d1-44ad-a013-18d0c29269e2>
2009-11-06 22:12:37 +0000

    Commiting untested code in comm for brick-to-brick BT communication.

--- dinaBOT/DinaBOTSlave.java
+++ dinaBOT/DinaBOTSlave.java
@@ -2,6 +2,7 @@ package dinaBOT;
 
 import lejos.nxt.Motor;
 import dinaBOT.mech.*;
+import dinaBOT.comm.*;
 
 /**
  * The DinaBOT class is the central class of our project. It ties everything togethere. It <b>is</b> the robot.
@@ -9,23 +10,68 @@ import dinaBOT.mech.*;
  * @author Alexandre Courtemanche, Francois Ouellet Delorme, Gabriel Olteanu, Severin Smith, Stepan Salenikovich, Vinh Phong Buu
 */
 public class DinaBOTSlave {
+	
+	public static final byte DO_NOTHING = 0;
+	public static final byte PICKUP = 1;
+	public static final byte OPEN_CAGE = 2;
+	public static final byte CLOSE_CAGE = 3;	
+	
 	Stacking stacker;
+	BTslave BTconnect;
 	
 	public DinaBOTSlave() {
 		stacker = new Stacker(Motor.A, Motor.B, Motor.C);
+		BTconnect = new BTslave();
 	}
 	/**
 	 * This is our Hello World method. It will be gone soon
 	 *
 	*/
 	public void test() {
-		stacker.activateMechanicalClaw();
+		
+		stacker.pickUp();
 		stacker.openDockingBay();
-		stacker.getDockStatus();
+		stacker.getCageStatus();
 		stacker.closeDockingBay();
-		stacker.getDockStatus();
+		stacker.getCageStatus();
 		
 	}
+	
+	public void obey() {
+		
+		byte nextCommand = 0;
+		boolean success = false;
+		
+		while(true) {
+			nextCommand = BTconnect.waitForCommand();
+			
+			switch (nextCommand) {
+					
+				case DO_NOTHING:
+					
+					break;
+				case PICKUP:
+					success = stacker.activateMechanicalClaw();
+					break;
+					
+				case OPEN_CAGE:
+					success = stacker.openDockingBay();
+					break;
+					
+				case CLOSE_CAGE:
+					success = stacker.closeDockingBay();
+					break;
+					
+			}
+			
+			BTconnect.sendStatus(success);
+			
+		}
+		
+		
+		
+	}
+	
 	/**
 	 * This is where the static main method lies. This is where execution begins.
 	 *
@@ -34,7 +80,24 @@ public class DinaBOTSlave {
 	public static void main(String[] args) {		
 		
 		DinaBOTSlave dinaBOTslave = new DinaBOTSlave();
-		dinaBOTslave.test();
-		while(true);
+		dinaBOTslave.obey();
+		
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
