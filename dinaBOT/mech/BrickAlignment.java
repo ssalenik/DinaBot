cf5a4d73db3ebcd03a1b4de905c5353f6b07fa53
Gabriel Olteanu <Gabriel Olteanu@f222f585-b4d1-44ad-a013-18d0c29269e2>
2009-11-06 21:25:04 +0000

    rough alignment

--- dinaBOT/mech/BrickAlignment.java
+++ dinaBOT/mech/BrickAlignment.java
@@ -1,5 +1,20 @@
 package dinaBOT.mech;
 
-public class BrickAlignment {
+import lejos.nxt.Motor;
 
+public class BrickAlignment {
+	
+	static void align(){
+		
+		Motor.A.setSpeed(300);
+		Motor.B.setSpeed(300);		
+		
+		Motor.A.rotate(720, true);
+		Motor.B.rotate(720);
+	}
+
+	public static void main(String[] args){
+		
+		align();
+	}
 }
