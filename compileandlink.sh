51a200b22c37fb844f78eef33b033ee0f2779fe9
alexlesuper <alexlesuper@f222f585-b4d1-44ad-a013-18d0c29269e2>
2009-11-06 22:37:47 +0000

    Updated compile script for trunk now with extra ghetto

--- compileandlink.sh
+++ compileandlink.sh
@@ -1,7 +1,26 @@
 #/bin/bash
 mkdir bin
-nxjc dinaBOT/DinaBOTMaster.java -d bin
-cd bin
-nxjlink dinaBOT/DinaBOTMaster -cp ./ -o ../DinaBOTMaster.nxj
-cd ..
-rm -rf bin
+if [ $1 = "m" ]
+then
+	nxjc dinaBOT/DinaBOTMaster.java -d bin
+	cd bin
+	nxjlink dinaBOT/DinaBOTMaster -cp ./ -o ../DinaBOTMaster.nxj
+	cd ..
+	rm -rf bin
+	if [ $2 = "f" ]
+	then
+		nxjupload DinaBOTSlave.nxj -u
+	fi
+fi
+if [ $1 = "s" ]
+then
+	nxjc dinaBOT/DinaBOTSlave.java -d bin
+	cd bin
+	nxjlink dinaBOT/DinaBOTSlave -cp ./ -o ../DinaBOTSlave.nxj
+	cd ..
+	rm -rf bin
+	if [ $2 = "f" ]
+	then
+		nxjupload DinaBOTSlave.nxj -u
+	fi
+fi
