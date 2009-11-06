2dc4fa7e13e7aa34177f746e399a670b7a2e5886
Gabriel Olteanu <Gabriel Olteanu@f222f585-b4d1-44ad-a013-18d0c29269e2>
2009-11-06 22:27:40 +0000

    align modified

--- dinaBOT/mech/BrickAlignment.java
+++ dinaBOT/mech/BrickAlignment.java
@@ -6,6 +6,14 @@ public class BrickAlignment {
 	
 	static void align(){
 		
+		Motor.A.setSpeed(100);
+		Motor.B.setSpeed(100);		
+		
+		Motor.A.rotate(204, true);
+		Motor.B.rotate(-204);
+		
+		//close claw
+		
 		Motor.A.setSpeed(300);
 		Motor.B.setSpeed(300);		
 		
